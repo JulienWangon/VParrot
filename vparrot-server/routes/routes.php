d19639d9648031818123912c0df0e0d3c60a73b3
Julien Wangon <julien.wangon@gmail.com>
2023-10-31 13:15:57 +0100

    ADD Create Router Skeleton

--- vparrot-server/routes/routes.php
+++ vparrot-server/routes/routes.php
@@ -9,3 +9,69 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 //Allowed headers (en-tête autorisés)
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
+
+
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+//LOADING CONTROLLERS
+$controllers = [];
+
+
+//ROUTES DEFINITION
+$routes = [
+    'GET' => [
+
+    ],
+
+    'POST' => [
+
+    ],
+
+    'PUT' => [
+
+    ],
+
+    'DELETE' => [
+
+    ]
+
+];
+
+
+//EXTRACTING THE URI FROM THE REQUEST
+$uri = str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['REQUEST_URI']);
+
+//ROUTING SYSTEM
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+$foundRoute = false;
+
+//Verification of Exact Routes
+if(isset($routes[$requestMethod][$uri])) {
+  $routes[$requestMethod][$uri]();
+  $foundRoute = true;
+}
+
+//If no exact route is found, check the routes with regex
+if (!$foundRoute) {
+
+    foreach($routes[$requestMethod] as $pattern =>$function) {
+        if (preg_match($pattern, $uri, $matches)) {
+          $function($matches);
+          $foundRoute = true;
+          break;
+        }
+
+    }
+
+}
+
+//IF ROUTE NOTE FOUND RETURN 404 ERROR
+if(!$foundRoute) {
+  header('HTTP/1.1 404 Not Found');
+  echo json_encode(['status' => 'error', 'message' => 'Route not found']);
+}
+
