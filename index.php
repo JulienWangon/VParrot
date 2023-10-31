77bb70ca16cf9e6239f184b176445e6eb22b3ead
Julien Wangon <julien.wangon@gmail.com>
2023-10-31 11:02:38 +0100

    ADD install firebase dependency with composer

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . '/vparrot-server/vendor/autoload.php';
+
+require_once './vparrot-server/routes/routes.php';
---
+++ vparrot-server/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0deaf0f23a9e5d8de71286d2639bc1f129852061',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0deaf0f23a9e5d8de71286d2639bc1f129852061',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.9.0',
+            'version' => '6.9.0.0',
+            'reference' => 'f03270e63eaccf3019ef0f32849c497385774e11',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
