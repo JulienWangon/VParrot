ad310a063c0c4053b032c08c262030a4a5e77bba
Julien Wangon <julien.wangon@gmail.com>
2023-10-31 13:53:23 +0100

    ADD create database connexion file

---
+++ vparrot-server/models/Database.php
@@ -0,0 +1,34 @@
+<?php
+// Classe non instanciable, seulement extenciable par nos model
+
+abstract class Database {
+//Stock l'instance unique de PDO
+    private static $pdo;
+//Nom de la table à définir
+    protected static $tableName = "";
+
+//Génère la connexion à la base de donnée
+    private static function setBdd() {
+        try {
+
+            self::$pdo = new PDO("mysql:host=localhost;dbname=vparrot_garage;charset=utf8", "root", "");
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+
+        } catch(PDOException $e) {
+//On enregistre l'erreur dans un fichier de log
+            error_log("Erreur de base de donnée: " . $e->getMessage() . " dans " . $e->getFile() . ' ligne ' . $e->getLine());
+//On affiche un message à l'utilisateur
+            throw new Exception(("Erreur de connexion à la base de donnée."));
+
+        }
+    }
+
+//Renvoi l'instance de PDO si l'instance n'est pas initialisé elle appel setBdd() pour le faire
+    protected function getBdd(){
+        if(self::$pdo === null){
+            self::setBdd();
+        }
+        return self::$pdo;
+        
+    }
+}
