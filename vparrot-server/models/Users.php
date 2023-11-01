c5b4adc03862edc934f49a807da42d4da9c25875
Julien Wangon <julien.wangon@gmail.com>
2023-11-01 17:08:58 +0100

    ADD create getters and setters list in Users Model

--- vparrot-server/models/Users.php
+++ vparrot-server/models/Users.php
@@ -21,6 +21,56 @@ class Users extends Database {
         $this->roleId = $roleId;
 
     }
+    
+    //Getters list
+    public function getIdUser() : int {
+        return $this->idUser;
+    }
+
+    public function getFirstName() :string {
+        return $this->firstName;
+    } 
+
+    public function getLastName() :string {
+        return $this->lastName;
+    }
+
+    public function getUserEmail() :string {
+        return $this->userEmail;
+    }
+
+    public function getUserPassword() :string {
+        return $this->userPassword;
+    }
+
+    public function gertRoleId() :int {
+        return $this->roleId;
+    }
+
+    //Setters list
+    public function setFirstName(string $firstName) :void {
+        $this->firstName = $firstName;
+        
+    }
+
+    public function setLastName(string $lastName) :void {
+        $this->lastName = $lastName;
+    }
+
+    public function setUserEmail(string $userEmail) :void {
+        $this->userEmail = $userEmail;
+    }
+
+    public function setUserPassword(string $userPassword) :void {
+        $this->userPassword = $userPassword;
+    }
+
+    public function setRoleId(int $roleId) :void {
+        $this->roleId = $roleId;
+    }
+
+    
+
 
 
 
