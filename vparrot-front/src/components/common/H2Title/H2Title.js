e8964bea6e2d22ce0a0cd10b836266fab3e677ac
Julien Wangon <julien.wangon@gmail.com>
2023-11-03 00:32:03 +0100

    ADD create H2title component

---
+++ vparrot-front/src/components/common/H2Title/H2Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TitleH2 = ({className, h2Text}) => {
+  return (
+    <h2 className={className}>
+        {h2Text}
+    </h2>
+  );
+};
+
+
+
+export default TitleH2;
