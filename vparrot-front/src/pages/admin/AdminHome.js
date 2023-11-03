09442ff254dd055ce2890cfee2e7396427f69e5b
Julien Wangon <julien.wangon@gmail.com>
2023-11-03 23:15:29 +0100

    ADD AdminHome.js : add header in AdminHome component

--- vparrot-front/src/pages/admin/AdminHome.js
+++ vparrot-front/src/pages/admin/AdminHome.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '../../components/common/Header/Header';
 
 const AdminHome = () => {
   return (
-    <div>
-      ICI PAGE D ACCEUIL ADMIN
-    </div>
+    <>
+        <Header title="Garage V.Parrot" slogan="Bienvenu sur votre espace administrateur"/>      
+    </>
   );
 };
 
