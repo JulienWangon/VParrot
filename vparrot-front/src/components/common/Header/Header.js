233d04e982d206af3a07179fc460b814297e9b02
Julien Wangon <julien.wangon@gmail.com>
2023-11-04 16:18:36 +0100

    MODIFY : Header.js delete navbar links logic

--- vparrot-front/src/components/common/Header/Header.js
+++ vparrot-front/src/components/common/Header/Header.js
@@ -1,52 +1,18 @@
-import React, { useState } from 'react';
-import { useAuth } from '../../../contexts/AuthContext';
-
+import React from 'react';
 
 import H1Title from '../H1Title/H1Title';
 import Navbar from '../Navbar/Navbar';
-import ConnexionModal from '../../public/ConnexionModal/ConnexionModal';
-
 
 
 const Header = ({ title, slogan}) => {
 
-  const [showModal, setShowModal] = useState(false);
-  const { currentUser, logout } = useAuth();
 
-  const toggleModal = () => {
-    setShowModal(!showModal);
-  };
-
-  const publicLinks = [
-    { text: 'Accueil', url: '/home' },
-    { text: 'Entretiens', url: '/entretiens' },
-    { text: 'Réparations', url: '/reparations' },
-    { text: 'Espace ventes', url: '/ventes' },
-    { text: 'Contact', url: '/contact' },
-    { text: 'Admin', action: toggleModal },
-  ];
-
-  const adminLinks = [
-
-    { text: 'Utilisateurs', url: '/manageusers' },
-    { text: 'Avis clients', url: '/managetestimonies' },
-    { text: 'Horaires', url: '/manageschedules' },
-    { text: 'Services', url: '/manageservices' },
-    { text: 'Parc automobile', url: '/managecars' },
-    { text: 'Deconnexion', action: logout },
-  ]
-
-  const links = currentUser ? adminLinks : publicLinks;
-  const headerClass = currentUser ? "adminHeader" : "publicHeader";
-
-
+  console.log('Header props:', { title, slogan });
   return (
-    <header className={headerClass}>
+    <header className="header">
         <div className="opacityLayer"></div>
-        <Navbar links={links}/>
-        <H1Title h1Text={title} spanText={slogan}/>
-        {showModal && <ConnexionModal closeModal={toggleModal} />}
-      
+        <Navbar/>
+        <H1Title h1Text={title} spanText={slogan}/>      
     </header>
   );
 };
