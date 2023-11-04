690c9dde615deda5ac5d06adc7caa6d2412d0b7b
Julien Wangon <julien.wangon@gmail.com>
2023-11-04 16:15:02 +0100

    MODIFY: ProtectedRoute component: adding timout for loading current user data

--- vparrot-front/src/_utils/ProtectedRoute.js
+++ vparrot-front/src/_utils/ProtectedRoute.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
-    const { currentUser } = useAuth();
+    const { currentUser, loading } = useAuth(); // Supposer qu'il y a un état de chargement dans useAuth
+    const [isReady, setIsReady] = useState(false);
 
-    if (currentUser) {
-        return children;
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsReady(true);
+        }, 100); // ajustez ce temps selon les besoins
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    // Vous attendez soit que l'état de chargement soit terminé, soit que le délai soit écoulé
+    if (loading && !isReady) {
+        return null; // ou votre indicateur de chargement discret ici
     }
 
-    return <Navigate to="/home" replace />;
+    if (!currentUser) {
+        return <Navigate to="/accueil" replace />;
+    }
+
+    return children;
 };
 
 export default ProtectedRoute;
