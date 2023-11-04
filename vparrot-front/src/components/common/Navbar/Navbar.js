47685a5ceac3ce603725fd2ad13717b60f468d04
Julien Wangon <julien.wangon@gmail.com>
2023-11-04 16:19:10 +0100

    ADD : Navbar.js add dynamic link logic

--- vparrot-front/src/components/common/Navbar/Navbar.js
+++ vparrot-front/src/components/common/Navbar/Navbar.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../../contexts/AuthContext';
+import Button from '../Button/Button';
 
-const Navbar = ({ links }) => {
+const Navbar = () => {
 
-    const handleAction = (action, e) => {
-        e.preventDefault();  // Prevent navigation
-        if (action) {
-            action();  // Execute the provided action if it exists
-        }
-    }
+    const { currentUser, logout} = useAuth();
+
+    //public links list
+    const publicLinks = [
+
+        { text: "Accueil", url: "/accueil" },
+        { text: "Entretiens", url: "/entretiens" },
+        { text: "Réparation", url: "/reparations" },
+        { text: "Espace vente", url: "/ventes" },
+
+    ];
+
+    //admin list links
+    const adminLinks = [
+
+        {text: "Accueil", url: "/accueiladmin"},
+        {text: "Utilisateurs", url: "/utilisateurs"},
+        {text: "Avis clients", url: "/avisclients"},
+        {text: "Horaires", url: "/horaires"},
+        {text: "Services", url: "/services"},
+        {text: "Parc auto", url: "/parcauto"},
+
+    ]
+
+    const links = currentUser ? adminLinks : publicLinks;
+
+
     
   return (
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -19,11 +42,19 @@ const Navbar = ({ links }) => {
               <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
 
                   <ul className="navbar-nav navMenu">
-                      {links.map((link, index) => (
-                          <li className="navItem" key={index}>
-                              {link.url ? ( <Link to={link.url} className="navLink">{link.text}</Link>) : ( <button onClick={(e) => {handleAction(link.action, e);}} className="navLink">{link.text}</button>)}
-                          </li>
-                      ))}
+                        {links.map((link, index) => (
+                            <li className="navItem" key={index}>
+                                {link.url ? ( <Link to={link.url} className="navLink">{link.text}</Link>) : ( <Button className="navLink" onClick={logout} colorStyle="whiteBtn">{link.text}</Button>)}
+                            </li>
+                        ))}
+
+                        {currentUser && (
+                            <li className="navItem">
+                                <Button className="navLink btn-danger" onClick={logout}>
+                                    Déconnexion
+                                </Button>
+                            </li>
+                        )}
                   </ul>
               </div>
           </div>        
