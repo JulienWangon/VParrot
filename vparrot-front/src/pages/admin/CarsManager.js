6d5ecf91eeda176027a04ba0b1217acf449abb6c
Julien Wangon <julien.wangon@gmail.com>
2023-11-04 16:25:33 +0100

    MODIFY : CarsManager.js : modify header title

--- vparrot-front/src/pages/admin/CarsManager.js
+++ vparrot-front/src/pages/admin/CarsManager.js
@@ -4,7 +4,7 @@ import Header from '../../components/common/Header/Header';
 const CarsManager = () => {
   return (
     <>
-      <Header title="Parc auto" slogan="Gestion des ventes de véhicules"/>
+      <Header title="Parc Auto" slogan="Gestion des ventes de véhicules"/>
       
     </>
   );
