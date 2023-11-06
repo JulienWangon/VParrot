1c11823f9ca214662d48b14c3f7dbab9a0f29641
Julien Wangon <julien.wangon@gmail.com>
2023-11-06 17:42:01 +0100

    ADD create H3 title component

---
+++ vparrot-front/src/components/common/H3Title/H3Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const H3Title = ({ className, h3Text }) => {
+  return (
+    <h3 className={className}>
+        {h3Text}      
+    </h3>
+  );
+};
+
+export default H3Title;
