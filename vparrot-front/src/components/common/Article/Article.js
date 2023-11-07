4a16038fb842d36f7fd0a2055357d941a1c1bc80
Julien Wangon <julien.wangon@gmail.com>
2023-11-07 02:50:35 +0100

    ADD create responsive design for home page component

--- vparrot-front/src/components/common/Article/Article.js
+++ vparrot-front/src/components/common/Article/Article.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import './article.css';
+
+
 const Article = ({ className, photo, altText, article }) => {
   return (
     <article className={className}>
       
-        <img className ="picture" src={photo} alt={altText}/>
-        <p>{article}</p>
+        <img className ="articlePhoto" src={photo} alt={altText}/>
+        <p className="articleText">{article}</p>
       
     </article>
   );
