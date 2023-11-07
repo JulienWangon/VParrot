6aa7c7f492090e5e291a869ef33b3ca91a58799d
Julien Wangon <julien.wangon@gmail.com>
2023-11-07 21:48:08 +0100

    create footer

--- vparrot-front/src/components/Footer/Footer.js
+++ vparrot-front/src/components/Footer/Footer.js
@@ -11,8 +11,9 @@ const Footer = () => {
 
   return (
     <footer>
-        <div className="schedulsContainer">
-            <H3Title className="schedulesTitle" h3Text="horaires d'ouverture"/>
+        <div className="opacityFooter"></div>
+        <div className="schedulesContainer">
+            <H3Title className="schedulesTitle" h3Text="Horaires d'ouverture"/>
             {loading ? (
                 <p>Chargement des horaires...</p>
             ) : error ? (
@@ -20,8 +21,8 @@ const Footer = () => {
             ) : (
                 <ul>
                     {schedules.map((schedule) => (
-                        <li key={schedules.id_opening_day}>
-                            <strong>{schedule.day_of_week.substring(0, 3)}:</strong> {schedule.morning_opening.substring(0. 5)} - {schedule.morning_closing.substring(0,5)} / {schedule.afternoon_opening.substring(0, 5)} - {schedule.afternoon_closing.substring(0, 5)} 
+                        <li key={schedule.id_opening_day}>
+                            <strong>{schedule.day_of_week.substring(0, 3)}:</strong> {schedule.morning_opening.substring(0, 5)} - {schedule.morning_closing.substring(0,5)} / {schedule.afternoon_opening.substring(0, 5)} - {schedule.afternoon_closing.substring(0, 5)} 
                         </li>
                     ))}
                 </ul>
@@ -33,7 +34,7 @@ const Footer = () => {
             <p className="phone">04.28.64.32.86</p>          
         </div>
         <div className="contactContainer">
-          
+
 
 
         </div>      
