3975db439561e46b6c4e83c8a514e1734e271da1
Julien Wangon <julien.wangon@gmail.com>
2023-11-08 23:24:41 +0100

    ADD : create hook fetch all services by type

---
+++ vparrot-front/src/components/Services/hooks/useServicesGroupedByType.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import { fetchAllServicesGroupedByType } from '../serviceServices';
+
+
+const useServicesGroupedByType  = () => {
+
+    const [servicesGroupedByType, setServiceGroupedByType] = useState({ entretien: [], carrosserie: [], mecanique: []});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+
+            try {
+
+                const data = await fetchAllServicesGroupedByType();
+                console.log("Données récupérées:", data);
+                setServiceGroupedByType(data);
+                setLoading(false);
+            } catch (error) {
+
+                setError(error.message);
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [])
+
+    return { servicesGroupedByType, loading, error };
+};
+
+export default useServicesGroupedByType;
