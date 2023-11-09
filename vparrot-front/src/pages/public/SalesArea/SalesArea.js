a1d6d470cc3569d85e6a84f80b02654a58ae3b80
Julien Wangon <julien.wangon@gmail.com>
2023-11-10 00:52:23 +0100

    ADD: add CarsCardSection component on SalesArea page

--- vparrot-front/src/pages/public/SalesArea/SalesArea.js
+++ vparrot-front/src/pages/public/SalesArea/SalesArea.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../../../components/common/Header/Header';
 
 import './salesArea.css';
+import CarsCardSection from '../../../components/Cars/public/CarsCardSection/CarsCardSection';
 
 const SalesArea = () => {
   return (
     <div>
       <Header title="Espace Vente" slogan="Véhicules d'occasions toutes marques"/>
+      <CarsCardSection/>
     </div>
   );
 };
