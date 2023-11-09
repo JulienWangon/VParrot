df22bbc26f02073ca87ce636e9437f3ebaeeb284
Julien Wangon <julien.wangon@gmail.com>
2023-11-09 02:51:54 +0100

    MODIFY: replace existing service section by new service section component

--- vparrot-front/src/pages/public/RepairServices/RepairServices.js
+++ vparrot-front/src/pages/public/RepairServices/RepairServices.js
@@ -4,8 +4,7 @@ import useServicesGroupedByType from '../../../components/Services/hooks/useServ
 
 import Header from '../../../components/common/Header/Header';
 import Footer from '../../../components/Footer/Footer';
-import ServiceCard from '../../../components/Services/public/ServiceCard/ServiceCard';
-import H2Title from '../../../components/common/H2Title/H2Title';
+import SectionService from '../../../components/Services/public/SectionService/SectionService';
 
 import './repairServices.css';
 
@@ -13,7 +12,7 @@ const RepairServices = () => {
 
   const { servicesGroupedByType, loading, error } = useServicesGroupedByType();
 
-
+  console.log(servicesGroupedByType.reparation);
   return (
     <>
       <Header title="Réparation" slogan="Réparation mécanique et carrosserie véhicules toutes marques"/>
@@ -30,68 +29,43 @@ const RepairServices = () => {
                   </p>
               </article>
           </div>
-          <section className="carrosserieSection">
-              <H2Title className="carrosserieTitle" h2Text="Services de Carrosserie V.Parrot"/>
-              <p className="carrosserieIntro">
-                  Chez V.Parrot, nous croyons que chaque voiture mérite de briller. 
-                  C'est pourquoi nous offrons des services de carrosserie de première classe, allant de la réparation de petites bosses à des travaux de restauration complets, pour redonner à votre véhicule son allure d'origine et garantir qu'il fait tourner les têtes sur la route.
-              </p>
-                  
-              {loading && <p>Chargement des services...</p>}
-              {error && <p>Erreur de chargement: {error}</p>}
-              {!loading && !error && (
-                  <div className="servicesGrid">
-                      {servicesGroupedByType.carrosserie.map((service) => (
-                          <ServiceCard
-                              key={service.id_service}
-                              name={service.service_name}
-                              description={service.description}
-                              price={service.price}
-                              pathImg={service.path_img}
-                          />
-                      ))}
-                  </div>
-              )}
 
-              <div className="articleCarrosserieContainer">
-                  <article className="carrosserieConclusion">
-                      <p>
-                          Chez V.Parrot, nous sommes fiers de notre capacité à remettre votre voiture à neuf, quelle que soit l'ampleur des travaux à réaliser. 
-                          Notre équipe de professionnels dédiés est à votre écoute pour répondre à tous vos besoins en matière de carrosserie. 
-                          Qu'il s'agisse de réparations mineures ou d'une restauration complète, nous mettons notre savoir-faire et notre passion au service de votre véhicule.
-                      </p>
-                      <p>
-                          Faites-nous confiance pour redonner à votre voiture son allure d'origine et garantir votre satisfaction. 
-                          Venez nous rendre visite chez V.Parrot et laissez-nous prendre soin de votre voiture comme si c'était la nôtre.
-                      </p>
-                  </article>
-              </div>
-          </section>
-          <section className="mecaniqueSection">
-              <H2Title className="mecaniqueTitle" h2Text="V.Parrot: Votre solution mécanique"/>
-              <p className="mecaniqueIntro">
-                  Chez V.Parrot, nous croyons que chaque voiture mérite le meilleur en matière de maintenance et de réparation mécanique.
-                  C'est pourquoi nous mettons à votre disposition une équipe de professionnels hautement qualifiés et expérimentés qui utilisent des équipements de pointe pour assurer la fiabilité et la performance de votre véhicule. 
-                  Qu'il s'agisse de petites réparations ou de problèmes mécaniques complexes, nous abordons chaque tâche avec le même souci du détail et la même passion pour l'excellence. 
-                  Faites confiance à V.Parrot pour tous vos besoins en réparation mécanique et laissez-nous veiller à ce que votre voiture soit toujours au sommet de ses performances.
-              </p>
+          <SectionService
+              title="Services de Carrosserie V.Parrot"
+              introduction="Chez V.Parrot, nous croyons que chaque voiture mérite de briller. 
+                            C'est pourquoi nous offrons des services de carrosserie de première classe, allant de la réparation de petites bosses à des travaux de restauration complets, pour redonner à votre véhicule son allure d'origine et garantir qu'il fait tourner les têtes sur la route.
+                            "
+              services={servicesGroupedByType.carrosserie}
+          />
+          {loading && <p>Chargement des services...</p>}
+          {error && <p>Erreur de chargement: {error}</p>}
+          
+          <div className="articleCarrosserieContainer">
+              <article className="carrosserieConclusion">
+                  <p>
+                      Chez V.Parrot, nous sommes fiers de notre capacité à remettre votre voiture à neuf, quelle que soit l'ampleur des travaux à réaliser. 
+                      Notre équipe de professionnels dédiés est à votre écoute pour répondre à tous vos besoins en matière de carrosserie. 
+                      Qu'il s'agisse de réparations mineures ou d'une restauration complète, nous mettons notre savoir-faire et notre passion au service de votre véhicule.
+                  </p>
+                  <p>
+                      Faites-nous confiance pour redonner à votre voiture son allure d'origine et garantir votre satisfaction. 
+                      Venez nous rendre visite chez V.Parrot et laissez-nous prendre soin de votre voiture comme si c'était la nôtre.
+                  </p>
+              </article>
+          </div>
 
-              {loading && <p>Chargement des services...</p>}
-              {error && <p>Erreur de chargement: {error}</p>}
-              {!loading && !error && (
-                  <div className="servicesGrid">
-                      {servicesGroupedByType.reparation.map((service) => (
-                          <ServiceCard
-                              key={service.id_service}
-                              name={service.service_name}
-                              description={service.description}
-                              price={service.price}
-                              pathImg={service.path_img}
-                          />
-                      ))}
-                  </div>
-              )}
-          </section>
+          <SectionService
+              title="V.Parrot: Votre solution mécanique"
+              introduction="Chez V.Parrot, nous croyons que chaque voiture mérite le meilleur en matière de maintenance et de réparation mécanique.
+                            C'est pourquoi nous mettons à votre disposition une équipe de professionnels hautement qualifiés et expérimentés qui utilisent des équipements de pointe pour assurer la fiabilité et la performance de votre véhicule. 
+                            Qu'il s'agisse de petites réparations ou de problèmes mécaniques complexes, nous abordons chaque tâche avec le même souci du détail et la même passion pour l'excellence. 
+                            Faites confiance à V.Parrot pour tous vos besoins en réparation mécanique et laissez-nous veiller à ce que votre voiture soit toujours au sommet de ses performances.
+                           "
+              services={servicesGroupedByType.reparation} 
+          />
+          {loading && <p>Chargement des services...</p>}
+          {error && <p>Erreur de chargement: {error}</p>}
+         
           <div className="articleRepairContainer">
               <article className="repairConclusion">
                   <p>
