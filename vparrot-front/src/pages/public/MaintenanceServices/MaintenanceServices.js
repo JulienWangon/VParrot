c1f0c6e35f81a2e59d525374a6ff4a00ce6e5d68
Julien Wangon <julien.wangon@gmail.com>
2023-11-09 01:13:17 +0100

    MODIFY: formating html  coe on maintenance page component

--- vparrot-front/src/pages/public/MaintenanceServices/MaintenanceServices.js
+++ vparrot-front/src/pages/public/MaintenanceServices/MaintenanceServices.js
@@ -18,11 +18,13 @@ const MaintenanceServices = () => {
       <main>
           <div className="articleMaintenanceContainer">
             <article className="maintenanceIntroduction">
-                <p>Bienvenue sur notre page de services d’entretien et de diagnostic chez V.Parrot. 
+                <p>
+                  Bienvenue sur notre page de services d’entretien et de diagnostic chez V.Parrot. 
                   En tant que leader dans le domaine de l'entretien automobile, nous sommes dédiés à fournir des services de la plus haute qualité pour garder votre véhicule en parfait état de marche. 
                   Que vous ayez besoin d'une simple vidange d'huile, d'un service complet de réparation des freins ou d'un diagnostic détaillé de votre véhicule, notre équipe de techniciens qualifiés est prête à répondre à vos besoins.
                 </p>
-                <p>Découvrez nos forfaits de services spécifiques ci-dessous et n'hésitez pas à nous contacter pour toute question ou pour planifier un rendez-vous. 
+                <p>
+                  Découvrez nos forfaits de services spécifiques ci-dessous et n'hésitez pas à nous contacter pour toute question ou pour planifier un rendez-vous. 
                   Nous sommes impatients de vous servir chez V.Parrot.
                 </p>
             </article>
