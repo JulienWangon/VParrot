27a54644d120b884c1e970f4ca3fb7165eaea347
Julien Wangon <julien.wangon@gmail.com>
2023-11-10 15:11:19 +0100

    ADD add methods to get cars features for select input

--- vparrot-server/repository/CarsRepository.php
+++ vparrot-server/repository/CarsRepository.php
@@ -27,4 +27,79 @@ class CarsRepository extends Database {
         }
     }
 
+    public function getDistinctBrands () {
+
+        try {
+
+            $db = $this->getBdd();
+
+            $req = "SELECT DISTINCT brand FROM cars";
+            $stmt = $db->prepare($req);
+            $stmt->execute();
+            $brands = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $brands;
+            
+        } catch (PDOException $e) {
+
+            $this->handleException($e, "extraction des marques distinctes");
+        }
+    }
+
+    public function getDistinctmModels () {
+
+        try {
+
+            $db = $this->getBdd();
+
+            $req = "SELECT DISTINCT model FROM cars";
+            $stmt = $db->prepare($req);
+            $stmt->execute();
+            $models = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $models;
+            
+        } catch (PDOException $e) {
+
+            $this->handleException($e, "extraction des marques distincts");
+        }
+    }
+
+    public function getDistinctFuelTypes () {
+
+        try {
+
+            $db = $this->getBdd();
+
+            $req = "SELECT DISTINCT fuel FROM car_features";
+            $stmt = $db->prepare($req);
+            $stmt->execute();
+            $fuleTypes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $fuleTypes;
+       
+        } catch (PDOException$e) {
+
+            $this->handleException($e, "extraction des carburants distincts");
+        }
+    }
+
+    public function getDistinctTransmissions () {
+
+        try {
+
+            $db = $this->getBdd();
+
+            $req = "SELECT DISTINCT transmission FROM car_features";
+            $stmt = $db->prepare($req);
+            $stmt->execute();
+            $fuleTypes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            return $fuleTypes;
+       
+        } catch (PDOException$e) {
+
+            $this->handleException($e, "extraction des transmissions distinctes");
+        }
+    }
 }
