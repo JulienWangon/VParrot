e8c57e5f48b6020c3e1921d7af0902f91535d2da
Julien Wangon <julien.wangon@gmail.com>
2023-11-10 16:09:06 +0100

    ADD: add fucntion to get distinct car features on Car service

--- vparrot-front/src/components/Cars/CarsService.js
+++ vparrot-front/src/components/Cars/CarsService.js
@@ -11,15 +11,88 @@ export const fetchCarsBriefDetails = async () => {
           return response.data.data;
         } else {
 
-            throw new Error("Données reçues non valides ou erreur de requête.");
+            throw new Error("Données reçues non valides ou erreur de requête. ");
         }
     } catch (error) {
 
-        console.error('Erreur lors de la récupération des détails des véhicules:', error);
+        console.error('Erreur lors de la récupération des détails des véhicules: ', error);
         throw error;
   }
 
 };
 
 //Get distinct brand
-export const fetchDistinctBrands = await 
+export const fetchDistinctBrands = async () => {
+    try {
+
+        const response = await instanceAxios.get('/cars/distinct-brands');
+        if (response.data && response.data.status === 'success') {
+
+            return response.data.data;
+        } else {
+
+            throw new Error("Données reçues non valides ou erreur de requête. ");
+        }
+    } catch (error) {
+
+        console.error('Erreur lors de la récupération des marques distinctes: ', error);
+        throw error;
+    }
+};
+
+//Get distinct model
+export const fetchDistinctModels = async () => {
+    try {
+
+        const response = await instanceAxios.get('/cars/distinct-models');
+        if (response.data && response.data.status === 'success') {
+
+            return response.data.data;
+        } else {
+
+            throw new Error("Données reçues non valides ou erreur de requête.");
+        }
+    } catch (error) {
+
+        console.error('Erreur lors de la récupération des modèle distincts: ', error);
+        throw error;
+    }
+};
+
+//Get distinct fuel types
+export const fetchDistinctFuelTypes = async () => {
+    try {
+
+        const response = await instanceAxios.get('/cars/distinct-fuel-types');
+        if (response.data && response.data.status === 'success') {
+
+            return response.data.data;
+        } else {
+
+            throw new Error("Données reçues non valides ou erreur de requête.");
+        }
+    } catch (error) {
+
+        console.error('Erreur lors de la récupération des types de carburants distincts: ', error);
+        throw error;
+    }
+};
+
+//Get distinct transmission types
+export const fetchDistinctTransmissionTypes = async () => {
+    try {
+
+        const response = await instanceAxios.get('/cars/distinct-transmission-types');
+        if (response.data && response.data.status === 'success') {
+
+            return response.data.data;
+        } else {
+
+            throw new Error("Données reçues non valides ou erreur de requête. ");
+        }
+    } catch (error) {
+
+        console.error('Erreur lors de la récupération des types de transmissions distinctes: ', error);
+        throw error;
+    }
+}
