26a2bcd2f6840496bb59c562b083bc54a6e5965a
Julien Wangon <julien.wangon@gmail.com>
2023-11-10 11:26:49 +0100

    TEST : deploiment test

--- vparrot-server/routes/routes.php
+++ vparrot-server/routes/routes.php
@@ -3,7 +3,7 @@
 //CORS configuration
 
 //localhost:3000 access (accès autorisé à l'API)
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: *");
 //allowed HTTP methods (méthode autorisées)
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 //Allowed headers (en-tête autorisés)
