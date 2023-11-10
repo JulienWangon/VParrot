cf1228ca1c9980303adaaa6e9c582f74b23e3dd0
Julien Wangon <julien.wangon@gmail.com>
2023-11-10 13:54:00 +0100

    ADD: create input text component

--- vparrot-front/src/components/common/TextInput/TextInput.js
+++ vparrot-front/src/components/common/TextInput/TextInput.js
@@ -1,26 +1,23 @@
 import React from 'react';
 
+const TextInput = ( {inputClassName, label, placeholder, name, type, value, autoComplete, onChange, error}) => {
 
-const TextInput = ({inputClassName, label, placeholder, name, type, value, autoComplete, onChange, error}) => {
-  return (
-    <div className="formGroup">
-        <label htmlFor={name}>{label}</label>
-        <input
-            className={inputClassName}
-            type={type}
-            name={name}
-            value={value}
-            autoComplete={autoComplete}
-            onChange={onChange}
-            placeholder={placeholder}        
-        />
-        {error && <div className="inputError">{error}</div>}    
-    </div>
-  );
+    return (
+        <div className='formGroup'>
+            <label htmlFor={name}>{label}</label>
+            <input
+                className={inputClassName}
+                type={type}
+                id={name}
+                name={name}
+                value={value}
+                autoComplete={autoComplete}
+                placeholder={placeholder}
+                onChange={onChange}                  
+            />
+            {error && <div className="inputError">{error}</div>}          
+        </div>
+    );
 };
-
-
-
-
 
 export default TextInput;
