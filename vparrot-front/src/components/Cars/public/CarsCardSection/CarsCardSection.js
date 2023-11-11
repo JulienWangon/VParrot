b7fbaf0211f3e3e5e55aa8219b5cea763a36a172
Julien Wangon <julien.wangon@gmail.com>
2023-11-11 17:30:12 +0100

    ADD add class on <p> if no car filtered

--- vparrot-front/src/components/Cars/public/CarsCardSection/CarsCardSection.js
+++ vparrot-front/src/components/Cars/public/CarsCardSection/CarsCardSection.js
@@ -28,7 +28,7 @@ const CarsCardSection = ({ filteredCars }) => {
                 carsBriefDetails.map((car) => <CarsCard key={car.id_car} car={car} />)
             ) : filteredCars.length === 0 ? (
                 // Afficher le message "aucun résultat" si filteredCars est un tableau vide
-                <p>Aucun véhicule ne correspond à vos critères de recherche.</p>
+                <p className="noCars">Aucun véhicule ne correspond à vos critères de recherche.</p>
             ) : (
                 // Afficher les voitures filtrées
                 filteredCars.map((car) => <CarsCard key={car.id_car} car={car} />)
