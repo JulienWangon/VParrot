199d63a7fa028300b62943b4293a988e0b87d316
Julien Wangon <julien.wangon@gmail.com>
2023-11-11 11:03:27 +0100

    ADD: add title on filter section

--- vparrot-front/src/components/Cars/public/CarFilters/CarFilters.js
+++ vparrot-front/src/components/Cars/public/CarFilters/CarFilters.js
@@ -6,6 +6,7 @@ import SelectInput from '../../../common/Input/SelectInput/SelectInput';
 import Button from '../../../common/Buttons/Button/Button';
 
 import './carFilters.css';
+import H2Title from '../../../common/H2Title/H2Title';
 
 const CarFilters = ({ onApplyFilter, onResetFilter}) => {
 
@@ -41,73 +42,75 @@ const CarFilters = ({ onApplyFilter, onResetFilter}) => {
     }
 
     return (
-      <section className="filtersSection">
+        <section className="filtersSection">
+        
+            <H2Title className="filtersTitle" h2Text="Filtres de recherche"/>
 
-          {loading && <p>Chargement des filtres...</p>}
-          {error && <p>Erreur de chargement: {error}</p>}
+            {loading && <p>Chargement des filtres...</p>}
+            {error && <p>Erreur de chargement: {error}</p>}
 
-          <div className="filtersList">
-              <SelectInput
-                  inputClassName="inputFilter"
-                  label="Marque"
-                  name="brand"
-                  value={filters.brand}
-                  onChange={handleChange}
-                  options={[{ id: '', value: '', label: 'Choisir une marque' }, ...filterData.brands.map(brandObj => ({ id: brandObj.brand, value: brandObj.brand, label: brandObj.brand }))]}
-              />
+            <div className="filtersList">
+                <SelectInput
+                    inputClassName="inputFilter"
+                    label="Marque"
+                    name="brand"
+                    value={filters.brand}
+                    onChange={handleChange}
+                    options={[{ id: '', value: '', label: 'Choisir une marque' }, ...filterData.brands.map(brandObj => ({ id: brandObj.brand, value: brandObj.brand, label: brandObj.brand }))]}
+                />
 
-              <SelectInput
-                  inputClassName="inputFilter"
-                  label="Modèle"
-                  name="model"
-                  value={filters.model}
-                  onChange={handleChange}
-                  options={[{ id: '', value: '', label: 'Choisir un modèle' }, ...filterData.models.map(modelObj => ({ id: modelObj.model, value: modelObj.model, label: modelObj.model }))]}
-              />
+                <SelectInput
+                    inputClassName="inputFilter"
+                    label="Modèle"
+                    name="model"
+                    value={filters.model}
+                    onChange={handleChange}
+                    options={[{ id: '', value: '', label: 'Choisir un modèle' }, ...filterData.models.map(modelObj => ({ id: modelObj.model, value: modelObj.model, label: modelObj.model }))]}
+                />
 
-              <SelectInput
-                  inputClassName="inputFilter"
-                  label="Carburant"
-                  name="fuel"
-                  value={filters.fuel}
-                  onChange={handleChange}
-                  options={[{ id: '', value: '', label: 'Choisir un carburant' }, ...filterData.fuelTypes.map(fuelObj => ({ id: fuelObj.fuel, value: fuelObj.fuel, label: fuelObj.fuel }))]}         
-              />
+                <SelectInput
+                    inputClassName="inputFilter"
+                    label="Carburant"
+                    name="fuel"
+                    value={filters.fuel}
+                    onChange={handleChange}
+                    options={[{ id: '', value: '', label: 'Choisir un carburant' }, ...filterData.fuelTypes.map(fuelObj => ({ id: fuelObj.fuel, value: fuelObj.fuel, label: fuelObj.fuel }))]}         
+                />
 
-              <SelectInput
-                  inputClassName="inputFilter"
-                  label="Transmission"
-                  name="transmission"
-                  value={filters.transmission}
-                  onChange={handleChange}
-                  options={[{ id: '', value: '', label: 'Choisir une transmission' }, ...filterData.transmissionTypes.map(transmissionObj => ({ id:transmissionObj.transmission, value: transmissionObj.transmission, label: transmissionObj.transmission }))]}    
-              />
+                <SelectInput
+                    inputClassName="inputFilter"
+                    label="Transmission"
+                    name="transmission"
+                    value={filters.transmission}
+                    onChange={handleChange}
+                    options={[{ id: '', value: '', label: 'Choisir une transmission' }, ...filterData.transmissionTypes.map(transmissionObj => ({ id:transmissionObj.transmission, value: transmissionObj.transmission, label: transmissionObj.transmission }))]}    
+                />
 
-              <TextInput
-                  inputClassName="inputFilter"
-                  label="Année Min"
-                  name="yearMin"
-                  type="number"
-                  value={filters.yearMin}
-                  onChange={handleChange}          
-              />
+                <TextInput
+                    inputClassName="inputFilter"
+                    label="Année Min"
+                    name="yearMin"
+                    type="number"
+                    value={filters.yearMin}
+                    onChange={handleChange}          
+                />
 
-              <TextInput
-                  inputClassName="inputFilter"
-                  label="Kilométrage Max"
-                  name="kmMax"
-                  type="number"
-                  value={filters.kmMax}
-                  onChange={handleChange}          
-              />
-          </div>
+                <TextInput
+                    inputClassName="inputFilter"
+                    label="Kilométrage Max"
+                    name="kmMax"
+                    type="number"
+                    value={filters.kmMax}
+                    onChange={handleChange}          
+                />
+            </div>
 
-          <div className="filterActions">
-                <Button onClick={handleSubmit} className="applyFilterBtn" colorStyle='redBtn'>Appliquer</Button>
-                <Button onClick={handleReset} className="resetFilterBtn" colorStyle='whiteBtn'>Réinitialiser</Button>
-          </div>
+            <div className="filterActions">
+                    <Button onClick={handleSubmit} className="applyFilterBtn" colorStyle='redBtn'>Appliquer</Button>
+                    <Button onClick={handleReset} className="resetFilterBtn" colorStyle='whiteBtn'>Réinitialiser</Button>
+            </div>
 
-      </section>
+        </section>
     );
 };
 
