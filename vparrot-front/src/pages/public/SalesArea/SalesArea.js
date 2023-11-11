4969ff19b362b5feb7fd2299c136a9855ead0e92
Julien Wangon <julien.wangon@gmail.com>
2023-11-11 11:01:36 +0100

    ADD : add components on Sales area page

--- vparrot-front/src/pages/public/SalesArea/SalesArea.js
+++ vparrot-front/src/pages/public/SalesArea/SalesArea.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import Header from '../../../components/common/Header/Header';
+import H2Title from '../../../components/common/H2Title/H2Title';
 import CarsCardSection from '../../../components/Cars/public/CarsCardSection/CarsCardSection';
 import CarFilters from '../../../components/Cars/public/CarFilters/CarFilters';
-
+import Footer from '../../../components/Footer/Footer';
 import useFetchFilteredCars from '../../../components/Cars/hooks/useFetchFileteredCars';
 
 import './salesArea.css';
@@ -16,32 +18,36 @@ const SalesArea = () => {
 
 
   return (
-    <>
+    <body>
         <Header title="Espace Vente" slogan="Véhicules d'occasions toutes marques"/>
+        <main>
+            <section>
+                <H2Title className="salesTitle" h2Text="L'occasion V.Parrot"/>
+                <article className="salesIntroduction">
+                    <p> 
+                        Bienvenue sur notre page de vente de véhicules d'occasion chez Garage V.Parrot ! 
+                        Nous sommes fiers de vous proposer une sélection exceptionnelle de voitures d'occasion de haute qualité pour répondre à tous vos besoins et préférences. 
+                        Que vous recherchiez une berline élégante, un SUV polyvalent, une compacte économique ou tout autre type de véhicule, nous avons ce qu'il vous faut. 
+                        Avec 15 ans d'expérience dans le domaine, nous mettons tout en œuvre pour vous offrir des véhicules fiables, minutieusement inspectés et prêts à prendre la route en toute confiance.
+                    </p>
+                    <p>
+                        Notre équipe experte est là pour vous guider tout au long du processus d'achat, en vous offrant des conseils avisés et un service personnalisé. 
+                        Parcourez notre inventaire en ligne et découvrez nos offres exceptionnelles. 
+                        Chez Garage V.Parrot, nous sommes impatients de vous aider à trouver le véhicule d'occasion de vos rêves !
+                    </p>
+                </article>
+            </section>
+            {error && <p>Erreur: {error}</p>}
 
-        <article className="salesIntroduction">
-            <p> 
-                Bienvenue sur notre page de vente de véhicules d'occasion chez Garage V.Parrot ! 
-                Nous sommes fiers de vous proposer une sélection exceptionnelle de voitures d'occasion de haute qualité pour répondre à tous vos besoins et préférences. 
-                Que vous recherchiez une berline élégante, un SUV polyvalent, une compacte économique ou tout autre type de véhicule, nous avons ce qu'il vous faut. 
-                Avec 15 ans d'expérience dans le domaine, nous mettons tout en œuvre pour vous offrir des véhicules fiables, minutieusement inspectés et prêts à prendre la route en toute confiance.
-            </p>
-            <p>
-                Notre équipe experte est là pour vous guider tout au long du processus d'achat, en vous offrant des conseils avisés et un service personnalisé. 
-                Parcourez notre inventaire en ligne et découvrez nos offres exceptionnelles. 
-                Chez Garage V.Parrot, nous sommes impatients de vous aider à trouver le véhicule d'occasion de vos rêves !
-            </p>
-        </article>
+            <CarFilters onApplyFilter={onApplyFilter} onResetFilter={resetFilter}/>
+
+            <CarsCardSection filteredCars={filteredCars}/>
+        </main>
+
+        <Footer/>
 
       
-        {error && <p>Erreur: {error}</p>}
-
-        <CarFilters onApplyFilter={onApplyFilter} onResetFilter={resetFilter}/>
-
-        <CarsCardSection filteredCars={filteredCars}/>
-
-      
-    </>
+    </body>
   );
 };
 
