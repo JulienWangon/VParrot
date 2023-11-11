e9539f54b6d83b3bd2e8c17f0fad2b5c9245db42
Julien Wangon <julien.wangon@gmail.com>
2023-11-11 20:19:57 +0100

    MODIFY :  modify method for formated data

--- vparrot-server/DTOs/CarDetailsDTO.php
+++ vparrot-server/DTOs/CarDetailsDTO.php
@@ -8,7 +8,7 @@ class CarDetailsDTO {
     public $idCar;
     public $brand;
     public $model;
-    public $trade_name;
+    public $tradeName;
     public $price;
     public $years;
     public $fuel;
@@ -18,13 +18,13 @@ class CarDetailsDTO {
     public $body;
     public $images;
     public $equipments;
-
+    
     public function __construct($data) {
 
-        $this->idCar = $data["id_car"];
+        $this->idCar = $data['id_car'];
         $this->brand = $data['brand'];
         $this->model = $data['model'];
-        $this->trade_name = $data['trade_name'];
+        $this->tradeName = $data['trade_name'];
         $this->price = $data['price'];
         $this->years = $data['years'];
         $this->fuel = $data['fuel'];
@@ -32,31 +32,39 @@ class CarDetailsDTO {
         $this->kilometer = $data['kilometer'];
         $this->transmission = $data['transmission'];
         $this->body = $data['body'];
-        $this->images = $this->modifyPathImage($data['images']);
-        $this->equipments = $this->parseEquipments($data['equipments']);
+        $this->images = $this->formatImages($data['images']);
+        $this->equipments = $this->formatEquipments($data['equipments']);
 
     }
 
-    private function parseEquipments($equipmentsString) {
-        $equipmentsArray = explode('; ', $equipmentsString);
-        $equipments = [];
+    private function formatEquipments($equipmentsString) {
+      $equipmentsArray = explode('; ', $equipmentsString);
+      $formattedEquipments = [];
 
-        foreach($equipmentsArray as $equipment) {
-            list($type, $name) = explode(":", $equipment);
-            $equipments[$type] = $name;
-        }
+      foreach($equipmentsArray as $equipment) {
+          [$type, $name] = explode(':', $equipment);
+          $formattedEquipments[$type][] = $name;
+      }
 
-        return $equipments;
+      // Supprimer les doublons pour chaque type
+      foreach($formattedEquipments as $type => $items) {
+          $formattedEquipments[$type] = array_unique($items);
+      }
+
+      return $formattedEquipments;
+  }
+
+
+  private function formatImages($imagesString) {
+    $images = explode('; ', $imagesString);
+    $images = array_unique($images); // Supprimer les doublons
+
+    // Ajouter BASE_PATH à chaque image
+    $formatedImages = [];
+    foreach ($images as $image) {
+        $formatedImages[] = BASE_PATH . $image;
     }
 
-    private function modifyPathImage($imagesString) {
-        $imagesPaths = explode('; ', $imagesString);
-
-        foreach($imagesPaths as $path) {
-            $pathImgModified[] = BASE_PATH . $path;
-        }
-
-        return $pathImgModified;
-    }
-
+    return $formatedImages;
+}
 }
