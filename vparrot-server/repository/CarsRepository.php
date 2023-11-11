5fa2c87dc33bb59ee1553fa06c89d8c64e69dbad
Julien Wangon <julien.wangon@gmail.com>
2023-11-11 17:29:07 +0100

    ADD: add method to get all car details

--- vparrot-server/repository/CarsRepository.php
+++ vparrot-server/repository/CarsRepository.php
@@ -8,22 +8,57 @@ class CarsRepository extends Database {
 
         try {
 
-          $db = $this->getBdd();
+            $db = $this->getBdd();
 
-          $req= "SELECT c.id_car, c.brand, c.model, c.trade_name, c.price, cf.years, cf.fuel, cf.power, cf.kilometer, cf.transmission, cf.body, ci.file_path AS image
-                 FROM cars c
-                 JOIN car_features cf ON c.id_car = cf.car_id
-                 LEFT JOIN cars_images ci ON c.id_car = ci.car_id AND ci.is_main = 1";
+            $req= "SELECT c.id_car, c.brand, c.model, c.trade_name, c.price, cf.years, cf.fuel, cf.power, cf.kilometer, cf.transmission, cf.body, ci.file_path AS image
+                    FROM cars c
+                    JOIN car_features cf ON c.id_car = cf.car_id
+                    LEFT JOIN cars_images ci ON c.id_car = ci.car_id AND ci.is_main = 1";
 
-          $stmt = $db->prepare($req);
-          $stmt->execute();
-          $cars = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $stmt = $db->prepare($req);
+            $stmt->execute();
+            $cars = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-          return $cars;
+             return $cars;
 
         } catch(PDOException $e) {
 
             $this->handleException($e, "extraction des informations de base des véhicules");
+        }
+    }
+
+
+    // Get all car details by car id 
+    public function getCarDetailsById($carId) {
+
+        try {
+
+            $db = $this->getBdd();
+            
+            $req = "SELECT
+                        c.*
+                        cf.years, cf.fuel, cf.power, cf.kilometer, cf.transmission, cf.body,
+                        GROUP_CONCAT(ci.file_path SEPARATOR '; ') AS images,
+                        GROUP_CONCAT(CONCAT(et.type_name, ':', e.denomination) SEPARATOR '; ' AS equipments
+                    FROM cars c
+                    LEFT JOIN car_features cf ON c.id_car = cf.car_id
+                    LEFT JOIN cars_images ci ON c.id_car = ci.car_id
+                    LEFT JOIN car_equipments ce ON c.id_car = ce.car_id
+                    LEFT JOIN equipments e ON ce.equipment_id = e.id_equipment
+                    LEFT JOIN equipment_types et ON e.type_id = et.id_type
+                    WHERE c.id_car = :carId
+                    GROUP BY c.id_car";
+
+                    $stmt = $db->prepare($req);
+                    $stmt->bindValue(":carId", $carId, PDO::PARAM_INT);
+                    $stmt->execute;
+                    $carDetails = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                    return $carDetails;
+
+        } catch (PDOException $e) {
+
+            $this->handleException($e, "extraction des détails d'un véhicule");
         }
     }
 
