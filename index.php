6a7186dc1202ffa2be6a527c1a790f219fe6ae76
Julien Wangon <julien.wangon@gmail.com>
2023-11-14 18:04:10 +0100

    UPDATE : update moulte config project

--- index.php
+++ index.php
@@ -2,4 +2,11 @@
 
 require __DIR__ . '/vparrot-server/vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/vparrot-server');
+$dotenv->load();
+
 require_once './vparrot-server/routes/routes.php';
+
+
+
+
--- vparrot-front/src/_utils/ProtectedRoute.js
+++ vparrot-front/src/_utils/ProtectedRoute.js
@@ -3,20 +3,20 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
-    const { currentUser, loading } = useAuth(); // Supposer qu'il y a un état de chargement dans useAuth
+    const { currentUser, loading } = useAuth();
     const [isReady, setIsReady] = useState(false);
 
     useEffect(() => {
         const timer = setTimeout(() => {
             setIsReady(true);
-        }, 100); // ajustez ce temps selon les besoins
+        }, 100); 
 
         return () => clearTimeout(timer);
     }, []);
 
-    // Vous attendez soit que l'état de chargement soit terminé, soit que le délai soit écoulé
+    
     if (loading && !isReady) {
-        return null; // ou votre indicateur de chargement discret ici
+        return null; 
     }
 
     if (!currentUser) {
