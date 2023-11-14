5369acf09f127db411138b076b4b44e9cd915ab1
Julien Wangon <julien.wangon@gmail.com>
2023-11-14 18:09:54 +0100

    UPDATE: add Repository on controller

--- vparrot-server/controllers/ServicesController.php
+++ vparrot-server/controllers/ServicesController.php
@@ -8,46 +8,57 @@ require_once __DIR__ . '/../repository/ServicesRepository.php';
   class ServicesController {
 
      
-      private $servicesRepository;
+    private $servicesRepository;
+    private $validator;
 
-      public function __construct(ServicesRepository $servicesRepository, Validator $validator) {
+    public function __construct(ServicesRepository $servicesRepository, Validator $validator) {
 
-          $this->servicesRepository = $servicesRepository;
-      }
+        $this->servicesRepository = $servicesRepository;
+        $this->validator = $validator;
+    }
 
-      private function sendResponse($data, $statusCode = 200) {
+    private function sendResponse($data, $statusCode = 200) {
 
-          header("Content-Type: application/json");
-          http_response_code($statusCode);
-          echo json_encode($data);
-          exit();
-      }
+        header("Content-Type: application/json");
+        http_response_code($statusCode);
+        echo json_encode($data);
+        exit();
+    }
 
       //Get all services list order by type
-      public function getAllServicesList() {
-          try {
+    public function getAllServicesListGroupedByType() {
+        try {
 
-              $services = $this->servicesRepository->getAllServicesGroupedByType();
+            $data = $this->servicesRepository->getAllServicesAndServiceTypes();
+            $services = $data['services'];
+            $serviceTypes = $data['serviceTypes'];
 
-              $groupedServices = [];
-              foreach ($services as &$service) {
-                // Ajouter le chemin de base aux images
-                if (isset($service['path_img']) && $service['path_img']) {
-                    $service['path_img'] = BASE_PATH . $service['path_img'];
+            $groupedServices = [];
+            foreach ($services as $service) {
+                $typeId = $service->getIdType();
+
+                if(isset($serviceTypes[$typeId])) {
+
+                    $typeName = $serviceTypes[$typeId]->getTypeName();
+                    $groupedServices[$typeName][] = [
+                        'idType' => $service->getIdType(),
+                        'typeName' => $serviceTypes[$typeId]->getTypeName(),
+                        'idService' => $service->getIdService(),
+                        'serviceName' => $service->getName(),
+                        'description' => $service->getDescription(),
+                        'price' => $service->getPrice(),
+                        'pathImg' => BASE_PATH . $service->getPathImg()
+                    ];
                 }
-    
-                $groupedServices[$service["type_name"]][] = $service;
             }
-    
-            $this->sendResponse(["status" => "success", "data" => $groupedServices]);
-              
-          } catch (Exception $e) {
 
-              $this->sendResponse([
-                  "status" => "error",
-                  "message" => $e->getMessage()
-              ], 500);
-          }
+            $this->sendResponse(['status' => 'success', 'data' => $groupedServices]);
+
+        } catch (Exception $e) {
+
+              $this->sendResponse(["status" => "error", "message" => $e->getMessage()], 500);
+                             
+        }
 
       }    
   }
