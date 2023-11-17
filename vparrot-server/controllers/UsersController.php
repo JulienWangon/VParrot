bb0f60e2e5924ed9a6f107433fbbb5c2c527afcf
Julien Wangon <julien.wangon@gmail.com>
2023-11-17 08:16:14 +0100

    add reset password request and reset password on controller

--- vparrot-server/controllers/UsersController.php
+++ vparrot-server/controllers/UsersController.php
@@ -2,20 +2,33 @@
 
 require_once './vparrot-server/models/Users.php';
 require_once './vparrot-server/Validator/Validator.php';
+require_once './vparrot-server/repository/UserRepository.php';
+require_once './vparrot-server/util/SecurityUtil.php';
+require_once './vparrot-server/util/EmailService.php';
+require_once './vparrot-server/models/AuthModel.php';
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 
 class UsersController {
 
     private $validator;
     private $users;
+    private $userRepository;
+    private $emailService;
+    private $authModel;
 
-    public function __construct($validator, $users) {
+    public function __construct(UserRepository $userRepository, Validator $validator, EmailService $emailService, AuthModel $authModel) {
         $this->validator = $validator;
-        $this->users = $users;
+        $this->userRepository = $userRepository;
+        $this->emailService = $emailService;
+        $this->authModel = $authModel;
     }
 
 
 
-    //Setting Response
+    //formatage de la réponse json
     private function sendResponse($data, $statusCode = 200) {
 
         header("Content-Type: application/json");
@@ -24,26 +37,26 @@ class UsersController {
 
     }
 
-    //GET all users list
+    //Obtenir la liste des utilisateurs et leur role
     public function getAllUsersList() {
-        $data = $this->users->getAllUsers();
+        $data = $this->userRepository->getAllUsersWithRoles();
         $this->sendResponse($data);
     }
 
-    //CREATE new user 
+    //Création d'un nouvel utilisateur
     public function addThisUser() {
 
-        // Retrieve data sent by the client
+        // Recupérer les données envoyé par l'utilisateur
         $data = json_decode(file_get_contents('php://input'), true);
 
-        //Validate json data format
+        //Valider le format json obtenu
         if (!$this->validator->validateJsonFormat($data)) {
 
             $this->sendResponse($this->validator->getErrors(), 400);
             return;
         }
 
-            //Check if all the necessary keys are present
+            //Vérifier si toute les clés sont présente
         $requireKeys =["first_name", "last_name", "user_email", "user_password", "role_id"];
         foreach ($requireKeys as $key) {
             if (!isset($data[$key])) {
@@ -53,14 +66,14 @@ class UsersController {
             }
         }
 
-        //Assign Data to Variables
+        //Assigne les données à des variable
         $firstName = $data["first_name"];
         $lastName = $data["last_name"];
         $userEmail = $data["user_email"];
         $userPassword = $data['user_password'];
         $roleId = $data["role_id"];
 
-        //Data validation
+        //validation des données
         $validFirstName = $this->validator->validateStringForNames($firstName, "prénom");
         $validLastName = $this->validator->validateStringForNames($lastName, "nom");
         $validUserEmail = $this->validator->validateEmail($userEmail);
@@ -74,19 +87,19 @@ class UsersController {
         }
 
     
-        //Check if e-mail exists
+        //Contrôle si l'email existe
         if($this->users->doesEmailExists($userEmail)) {
 
             $this->sendResponse(["status" => "error", "message" => "L'adresse e-mail existe déjà."]);
             return;
          }
 
-        //Hash password
+        //Hash du mot de passe
         $hashedPassword = password_hash($userPassword, PASSWORD_DEFAULT);
 
         try {
 
-            if ($this->users->addUser($firstName, $lastName, $userEmail, $userPassword, $roleId)) {
+            if ($this->userRepository->addUser($firstName, $lastName, $userEmail, $userPassword, $roleId)) {
                 $this->sendResponse(["status" => "success", "message" => "Utilisateur créé avec succès"], 200);
 
             } else {
@@ -101,13 +114,13 @@ class UsersController {
         
     }
 
-    //UPDATE User
+    //Mise à jour utilisateur
     public function updateThisUser(int $idUser) {
 
-        // Retrieve data sent by the client
+        //Récupération des données du formulaire
         $data = json_decode(file_get_contents('php://input'), true);
 
-        //Validate json data format
+        //Validation du format json
         if (!$this->validator->validateJsonFormat($data)) {
 
             $this->sendResponse($this->validator->getErrors(), 400);
@@ -123,13 +136,13 @@ class UsersController {
                 }
             }
 
-        //Assign Data to Variables
+    
         $firstName = $data["first_name"];
         $lastName = $data["last_name"];
         $userEmail = $data["user_email"];
         $roleId = $data["role_id"];
 
-        //Data validation
+        //Validation du format des données
         $validFirstName = $this->validator->validateStringForNames($firstName, "prénom");
         $validLastName = $this->validator->validateStringForNames($lastName, "nom");
         $validUserEmail = $this->validator->validateEmail($userEmail);
@@ -141,13 +154,13 @@ class UsersController {
             return;
           }
 
-        // Check if user exists before updating
+        // Vérifie si l'utilisateur existe avant la mise  jour
         if(!$this->users->doesUserExist($idUser)) {
             $this->sendResponse(["status" => "error", "message" => "L'utilisateur n'existe pas."], 404); // 404 Not Found
             return;
         }
 
-        // Check if e-mail is taken by another user (excluding the current user being updated).
+        // Vérifie si l email est déja utilisé par un autre utilisateur
         if($this->users->doesEmailExists($userEmail, $idUser)) {
 
             $this->sendResponse(["status" => "error", "message" => "L'adresse e-mail est déjà utilisée par un autre utilisateur."]);
@@ -156,7 +169,7 @@ class UsersController {
 
         try {
 
-            if ($this->users->updateUser($idUser, $firstName, $lastName, $userEmail, $roleId)) {
+            if ($this->userRepository->updateUser($idUser, $firstName, $lastName, $userEmail, $roleId)) {
                 $this->sendResponse(["status" => "success", "message" => "Utilisateur mis à jour avec succès"], 200);
 
             } else {
@@ -167,7 +180,167 @@ class UsersController {
         } catch(Exception $e) {
             $this->sendResponse(["status" => "error", "message" => $e->getMessage()], 500);
         }
-
     }
-    
+
+
+     /**
+     * Gère la demande de réinitialisation du mot de passe.
+     *
+     * @param string $email Email de l'utilisateur demandant la réinitialisation.
+     */
+    public function requestPasswordReset() {
+
+         // Récupération des données envoyées par le client
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        // Vérification si l'adresse email est fournie dans les données
+        if (!isset($data['email'])) {
+            // Si l'adresse email n'est pas fournie, renvoyer une réponse d'erreur
+            $this->sendResponse(["status" => "error", "message" => "L'adresse email est manquante"], 400);
+            return;
+        }
+
+        // Récupération de l'adresse email depuis les données
+        $email = $data['email'];
+        $captchaToken = $data['captchaToken'];
+
+        //validation des formats
+        $validUserEmail = $this->validator->validateEmail($email);
+        $validCaptchaToken = $this->validator->verifyGoogleCaptcha($captchaToken);
+
+        if(!$validUserEmail) {
+
+            $errors = $this->validator->getErrors();
+            $this->sendResponse(["status" => "error", "message" => $errors], 400);
+            return;
+        }
+
+        if(!$validCaptchaToken) {
+            $errors = $this->validator->getErrors();
+            $this->sendResponse(['status' => 'error', 'message' => $errors], 401);
+            return;
+
+        }
+
+
+        //vérification si l'utilisateur existedans la bdd
+        $user = $this->userRepository->doesEmailExist($email);
+
+        if(!$user) {
+
+            $this->sendResponse(['message' => 'Si votre email est reconnue, un email de réinitialisation de mot de passe vous sera envoyé. ']);
+            return;
+        }
+
+        //Générer un token de réinitialisation
+        $issuedAt = time();
+        $expirationTime = $issuedAt + (20*60);
+
+        $payload = [
+            'iat' => $issuedAt,
+            'exp' => $expirationTime
+        ];
+
+        $jwt = JWT::encode($payload, $_ENV['SECRET_KEY'], "HS256");
+
+        //Création du lien de renouvellement de mot de passe 
+        $resetLink = FRONT_BASE_PATH . "reset-password?token=$jwt";
+
+        //Préparation de l'email 
+        $to = $email;
+        $subject = "Réinitialisation de votre mot de passe";
+        $body = "Vous avez demandé la réinitialisation de votre mot de passe, cliquez sur le lien suivant pour accéder à la page de réinitialisation : <a href='{$resetLink}'>Réinitialiser mon mot de passe</a>
+                Ce lien n'est valable que pour 20 minutes.";
+        $isHTML = true;
+
+        try {
+        
+            //Utilisation du service email pour envoi du mail de changement de mot de passe
+            $this->emailService->sendEmail($to, $subject, $body, $isHTML);
+            $this->sendResponse(["message" => "Un email de réinitialisation de mot de passe a étét envoyé. "]);   
+        } catch (Exception $e) {
+
+            $this->sendResponse(["status" => "error", "message" => $e->getMessage()], 500);
+        }
+    }
+
+
+    public function resetPassword() {
+
+        
+        try {
+
+            $data = json_decode(file_get_contents('php://input'), true);
+            //Vérifie la présence des données nécéssaires
+            if(!isset($data['email']) || !isset($data['newPassword']) || !isset($data['captchaValue']) || !isset($data['token'])) {
+                $this->sendResponse(["status" => "error", "message"=> "Donnée incomplète pour le changement du mot de passe"]);
+                return;
+            }
+
+            $email = $data['email'];
+            $newPassword = $data['newPassword'];
+            $captchaToken = $data['captchaValue'];
+            $token = $data['token'];
+
+            $decoded = JWT::decode($token, new Key($_ENV['SECRET_KEY'], 'HS256'));
+
+        // Vérification de l'expiration du token
+        if (time() > $decoded->exp) {
+            $this->sendResponse(["status" => "error", "message" => "Le token de réinitialisation a expiré"], 401);
+            return;
+        }
+           
+            
+
+            //Vérifier le format de l'email
+            if(!$this->validator->validateEmail($email)) {
+                $this->sendResponse(["status" => "error", "message"=> "Le format d'email est invalide."], 400);
+                return;
+            }
+
+            //Vérifier le format du mot de passe
+            if(!$this->validator->validatePassword($newPassword)) {
+                $this->sendResponse(["status" => "error", "message"=> "Le format du mot de passe est invalide. "], 400);
+                return;
+            }
+
+            //vérifier le captcha
+            if (!$this->validator->verifyGoogleCaptcha($captchaToken)) {
+                $this->sendResponse(["status" => "error", "message" => "Échec de la validation du CAPTCHA"], 400);
+                return;
+            }
+
+
+            //Vérifier si l'email existe dans la base de donnée 
+            if(!$this->userRepository->doesEmailExist($email)) {
+                $this->sendResponse(["status" => "error", "message"=> "L'utilisateur n'existe pas."], 404);
+                return;
+            }
+
+           
+            //Hashage du nouveau mot de passe
+            $hashednewPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+            $passwordChanged = $this->userRepository->updateUserPassword($email, $hashednewPassword);
+
+            if(!$passwordChanged) {
+                $this->sendResponse(["status" => "error", "message"=> "impossible de changer le mot de passe. "], 500);
+                return;
+            }
+
+            //Préparation de l'email envoyé à l'utilisateur
+            $to = $email;
+            $subject = "Changement de votre mot de passe.";
+            $body = "Votre mot de passe vient d'être changé. Si vous n'avez pas effectué cette modification, veuillez contacter votre administrateur.";
+            $isHTML = true;
+
+            //Envoi de l'email à l'utilisateur
+            $this->emailService->sendEmail($to, $subject, $body, $isHTML);
+            
+            $this->sendResponse(["status" => "success", "message" => "Mot de passe changé avec succès"]);
+        } catch (Exception $e) {
+
+            $this->sendResponse(["status" => "error", "message" => "Erreur lors du changement de mot de passe: " . $e->getMessage()], 500);
+        }
+    }
+   
 }
