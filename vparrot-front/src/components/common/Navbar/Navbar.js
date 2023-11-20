cdae51de11b6589ab059f35ce9c9869f55d03c9f
Julien Wangon <julien.wangon@gmail.com>
2023-11-20 04:22:26 +0100

    ADD add protecged route rules on header

--- vparrot-front/src/components/common/Navbar/Navbar.js
+++ vparrot-front/src/components/common/Navbar/Navbar.js
@@ -1,9 +1,9 @@
-import React from 'react';
-
-//Components import
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
 import Button from '../Buttons/Button/Button';
+import UnauthorizedModal from './UnauthorizedModal/UnauthorizedModal';
 
 //CSS import
 import './navbar.css';
@@ -11,6 +11,26 @@ import './navbar.css';
 const Navbar = () => {
 
     const { currentUser, logout} = useAuth();
+    const [showUnauthorizedModal, setShowUnauthorizedModal] = useState(false);
+    const navigate = useNavigate();
+
+    const adminOnlyLinks = ["Utilisateurs", "Horaires", "Services"];
+
+    const checkAuthorization = (linkText) => {
+        if (adminOnlyLinks.includes(linkText) && currentUser?.role !== 'admin') {
+            setShowUnauthorizedModal(true);
+            return false;
+        }
+        return true;
+    };
+
+    const handleLinkClick = (linkText, url) => {
+        // Si le lien ne nécessite pas de vérification d'autorisation, ou si l'utilisateur est autorisé
+        if (!adminOnlyLinks.includes(linkText) || checkAuthorization(linkText)) {
+            navigate(url);
+        }
+    };
+    
 
     //public links list
     const publicLinks = [
@@ -39,31 +59,50 @@ const Navbar = () => {
 
     
   return (
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-              <button className="navbar-toggler ma-toggler-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                  <i className="fa-solid fa-bars"></i>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <button className="navbar-toggler ma-toggler-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <i className="fa-solid fa-bars"></i>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
 
-                  <ul className="navbar-nav navMenu">
-                        {links.map((link, index) => (
-                            <li className="navItem" key={index}>
-                                {link.url ? ( <Link to={link.url} className="navLink">{link.text}</Link>) : ( <Button className="navLink" onClick={logout} colorStyle="whiteBtn">{link.text}</Button>)}
-                            </li>
-                        ))}
+                    <ul className="navbar-nav navMenu">
+                    {links.map((link, index) => (
+                        <li className="navItem" key={index}>
+                            <a 
+                                href={link.url} 
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    handleLinkClick(link.text, link.url);
+                                }} 
+                                className="navLink"
+                            >
+                                {link.text}
+                            </a>
+                        </li>
+                    ))}
+                    {currentUser && (
+                        <li className="navItem">
+                            <Button className="navLink btn-danger" onClick={logout}>
+                                 Déconnexion
+                            </Button>
+                         </li>
+                    )}
+                    </ul>
+                </div>
+            </div>        
+        </nav>
 
-                        {currentUser && (
-                            <li className="navItem">
-                                <Button className="navLink btn-danger" onClick={logout}>
-                                    Déconnexion
-                                </Button>
-                            </li>
-                        )}
-                  </ul>
-              </div>
-          </div>        
-      </nav>
+        {showUnauthorizedModal && (
+            <UnauthorizedModal
+                title="Accès Non Autorisé"
+                onClose={() => setShowUnauthorizedModal(false)}
+            >
+                <p>Vous n'avez pas les autorisations nécessaires pour accéder à cette fonctionnalité.</p>
+            </UnauthorizedModal>
+        )}
+    </>
   );
 };
 
