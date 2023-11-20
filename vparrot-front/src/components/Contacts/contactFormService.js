75246ddae076801e23ba06a94a69df4ea30ba2cc
Julien Wangon <julien.wangon@gmail.com>
2023-11-21 00:44:40 +0100

    ADD create get all contact on contact service

--- vparrot-front/src/components/Contacts/contactFormService.js
+++ vparrot-front/src/components/Contacts/contactFormService.js
@@ -13,7 +13,27 @@ export const sendContactFormData = async (formData) => {
       }
     } catch (error) {
 
-        console.error("Erreur lors de l’envoi des données du formulaire de contact:', error");
+        console.error("Erreur lors de l’envoi des données du formulaire de contact", error);
+        throw error
+    }
+
+}
+
+
+export const fetchAllContact = async () => {
+    try {
+
+        const response = await instanceAxios.get('/contact');
+        if(response.data && response.data.status === "success") {
+
+            return response.data.data
+        } else {
+
+            throw new Error("impossible d'obtenir la liste des contacts");
+        }   
+    } catch (error) {
+
+        console.error("Erreur lors de la récupération de la liste de contact", error);
         throw error
     }
 
