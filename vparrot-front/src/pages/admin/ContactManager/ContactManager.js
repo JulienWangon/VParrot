8cdde54839a321f62a83eb6f34e0f6b4d8a26f51
Julien Wangon <julien.wangon@gmail.com>
2023-11-21 00:45:33 +0100

    ADD create contact page

---
+++ vparrot-front/src/pages/admin/ContactManager/ContactManager.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../../components/common/Header/Header';
+
+const ContactManager = () => {
+  return (
+    <>
+      <Header title="Contacts" slogan="Gestion des contacts"/>
+      
+    </>
+  );
+};
+
+export default ContactManager;
