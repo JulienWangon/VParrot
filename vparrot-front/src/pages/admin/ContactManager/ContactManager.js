6035f1df7930132833ad624c69b8cfc777d7c4fb
Julien Wangon <julien.wangon@gmail.com>
2023-11-21 01:20:00 +0100

    ADD add contactSection on ContactManager

--- vparrot-front/src/pages/admin/ContactManager/ContactManager.js
+++ vparrot-front/src/pages/admin/ContactManager/ContactManager.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from '../../../components/common/Header/Header';
+import ContactSection from '../../../components/Contacts/admin/ContactSection/ContactSection';
+
 
 const ContactManager = () => {
   return (
     <>
       <Header title="Contacts" slogan="Gestion des contacts"/>
+      <main>
+          <ContactSection/>
+
+
+      </main>
       
     </>
   );
