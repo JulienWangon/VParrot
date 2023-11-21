956aec8a4b12eee5006c83481271f91dafc8d148
Julien Wangon <julien.wangon@gmail.com>
2023-11-21 01:20:20 +0100

    ADD create contactSection

---
+++ vparrot-front/src/components/Contacts/admin/ContactSection/ContactSection.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import useFetchallContact from '../../hooks/useFetchAllContact';
+import ContactCard from '../ContactCard/ContactCard';
+
+const ContactSection = () => {
+
+    const { contacts, loading, error } = useFetchallContact();
+
+    if (loading) return <div>Chargement...</div>
+    if(error) return <div>Erreur: {error}</div>
+
+    return (
+      <section className="container mt-4">
+            <div className="row row-cols-1 row-cols-md-2 g-4">
+                {contacts.map(contact => (
+                    <div className="col d-flex align-items-stretch">
+                        <ContactCard contact={contact} />
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default ContactSection;
