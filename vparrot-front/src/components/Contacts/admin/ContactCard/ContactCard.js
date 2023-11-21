56934627ce9c04e46605be125c0094cc111e4276
Julien Wangon <julien.wangon@gmail.com>
2023-11-21 01:20:45 +0100

    UPDATE : add bootstrap responsiv class

--- vparrot-front/src/components/Contacts/admin/ContactCard/ContactCard.js
+++ vparrot-front/src/components/Contacts/admin/ContactCard/ContactCard.js
@@ -3,16 +3,16 @@ import Button from '../../../common/Buttons/Button/Button';
 
 const ContactCard = ( { contact }) => {
     return (
-        <div className="cardContact">
-            <h5 className="cardcontactHeader">{contact.contactSubject}</h5>
-            <div className="cardContactBody">
-                <h5 className="cardContactTitle">{`${contact.lastName} ${contact.firstName}`}</h5>
-                <p className="cardContactContent">{contact.content}</p>
-                <Button>Traiter le Message</Button>   
+        <div className="cardContact card my-3">
+            <h5 className="cardcontactHeader card-header">{contact.contactSubject}</h5>
+            <div className="cardContactBody card-body">
+                <h5 className="cardContactTitle card-title">{`${contact.lastName} ${contact.firstName}`}</h5>
+                <p className="cardContactContent card-text">{contact.content}</p>
+                <Button className="contactTraitment" colorStyle="redBtn">Traiter le Message</Button>   
             </div>
-            <div class="cardContactFooter">
-                <span className="emailContact">{contact.email}</span>
-                <span className="phoneContact">{contact.phone}</span>      
+            <div class="cardContactFooter card-footer text-muted">
+                <span className="emailContact me-3">{contact.email}</span>
+                <span className="phoneContact">0{contact.phone}</span>      
             </div>
         </div>
     );
