c55a028be9ccee921b4982f2fa8e70223a72e29d
Julien Wangon <julien.wangon@gmail.com>
2023-11-23 19:49:20 +0100

    UPDATE add class for white title

--- vparrot-front/src/components/common/H2Title/H2Title.js
+++ vparrot-front/src/components/common/H2Title/H2Title.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './h2Title.css';
 
-const H2Title = ({className, h2Text}) => {
+const H2Title = ({className, h2Text, colorStyle}) => {
   return (
-    <h2 className={className}>
+    <h2 className={`${className} ${colorStyle}`}>
         {h2Text}
     </h2>
   );
