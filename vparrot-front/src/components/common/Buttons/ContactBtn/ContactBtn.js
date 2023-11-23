4aa29debb76e02ac01f34dfd9e12754e3e22fdbf
Julien Wangon <julien.wangon@gmail.com>
2023-11-23 17:58:14 +0100

    UPDATE : add contactModalContext gestion on contact button

--- vparrot-front/src/components/common/Buttons/ContactBtn/ContactBtn.js
+++ vparrot-front/src/components/common/Buttons/ContactBtn/ContactBtn.js
@@ -3,12 +3,21 @@ import React from 'react';
 import Button from '../Button/Button';
 
 import './contactBtn.css';
+import { useContactModal } from '../../../../contexts/ContactModalContext';
 
-const ContactBtn = ( {onClick}) => {
+const ContactBtn = ( { additionalData }) => {
+
+    const { openContactModal } = useContactModal();
+  
+    const handleClick = () => {
+      console.log("Données supplémentaires passées à ContactBtn:", additionalData);
+        openContactModal(additionalData || {});
+    }
+
   return (
     <>
 
-        <Button className="contactBtn" colorStyle="redBtn" onClick={onClick}>Contact</Button>
+        <Button className="contactBtn" colorStyle="redBtn" onClick={handleClick}>Contact</Button>
       
     </>
   );
