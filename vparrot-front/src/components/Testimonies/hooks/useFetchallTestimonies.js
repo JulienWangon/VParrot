1564fca5ce001dacfd1bc17208fc154623da4986
Julien Wangon <julien.wangon@gmail.com>
2023-11-28 00:52:29 +0100

    ADD create useFetchallTestimonies hook

---
+++ vparrot-front/src/components/Testimonies/hooks/useFetchallTestimonies.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import { fetchAllTestimonies } from "../testimoniesService";
+import { useMessage } from "../../../contexts/MessagesContext";
+
+
+const useFetchAllTestimonies = () => {
+
+    const [testimonies, setTestimonies] = useState({ "en attente": [], "approuvé": [], "rejeté": []});
+    const [isLoading, setIsLoading] = useState(false);
+    const { showMessage } = useMessage();
+
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+
+            setIsLoading(true);
+
+            try {
+                
+                const response = await fetchAllTestimonies();
+                setTestimonies(response);
+  
+            } catch (error) {
+
+                if (error.response && error.response.data && error.response.data.status === 'error') {
+
+                // Utilisation du message d'erreur de l'API
+                showMessage({ message: error.response.data.message || "Erreur lors de l'opération" }, 'error');
+                } else {
+
+                // Gestion des autres types d'erreurs (par exemple, erreur réseau)
+                showMessage({ message: error.message || "Une erreur s'est produite" }, 'error');
+                }
+            } finally {
+
+              setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [showMessage]);
+
+    return { testimonies, isLoading};
+}
+
+export default useFetchAllTestimonies;
