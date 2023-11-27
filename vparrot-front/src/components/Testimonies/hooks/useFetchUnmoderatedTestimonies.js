566ac6069547c7bbd5a86982d4e08f5909b2c986
Julien Wangon <julien.wangon@gmail.com>
2023-11-27 10:43:40 +0100

    UPDATE : change format fot fetch data

--- vparrot-front/src/components/Testimonies/hooks/useFetchUnmoderatedTestimonies.js
+++ vparrot-front/src/components/Testimonies/hooks/useFetchUnmoderatedTestimonies.js
@@ -13,29 +13,24 @@ const useGetUnmoderatedTestimonies = () => {
      // État pour gérer les erreurs potentielles.
      const [error, setError] = useState(null);
 
+     const fetchData = async () => {
+        setLoading(true);
+        try {
+            const unModeratedTestimonies = await getUnmoderatedTestimonies();
+            setTestimonies(unModeratedTestimonies.data);
+        } catch (error) {
+            setError(error.message || "Une erreur est survenue lors de la récupération des témoignages.");
+        } finally {
+            setLoading(false);
+        }
+    };
+
     useEffect(() => {
-        //Focntion asynchrone pour récupérer les avis client non modérés
-        const testimoniesForModeration = async () => {
-
-            try {
-                //Appelle a la fonction de service pour récupérer la liste des avis client
-                const unModeratedTestimonies = await getUnmoderatedTestimonies();
-                //Mise à jour de létat avec les données récupérées
-                setTestimonies(unModeratedTestimonies.data);   
-            } catch (error) {
-                
-                setError(error.message || "Une erreur est survenue lors de la récupération des témoignages." );
-                throw error;
-
-            } finally {
-                //Désactivation de l'état de chargement
-                setLoading(false);   
-            }
-          };
-
-        //Appelle a la fonction au montage du composant
-        testimoniesForModeration();
+        fetchData();
     }, []);
+
+
+
     //Retourne les avis non modérés l état de chargement et les erreurs
     return { testimonies, loading, error};   
 };
