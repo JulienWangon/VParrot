3fadea0c8860520b37789fe3ca9127f0c0a34f59
Julien Wangon <julien.wangon@gmail.com>
2023-11-27 10:49:35 +0100

    UPDATE : add className on JSX for use CSS rules

--- vparrot-front/src/components/Testimonies/admin/RejectedTestimonies/RejectedTestimonies.js
+++ vparrot-front/src/components/Testimonies/admin/RejectedTestimonies/RejectedTestimonies.js
@@ -1,16 +1,14 @@
 import React, { useState } from 'react';
-import useFetchRejectedTestimonies from '../../hooks/useFetchRejectedTestimonies';
+
 import TestimonyTable from '../TestimonyTab/TestimonyTable';
 import TestimonyModal from '../../public/TestimonyModal/TestimonyModal';
 
-const RejectedTestimoniesSection = () => {
+const RejectedTestimoniesSection = ({ testimonies }) => {
 
-    const { testimonies, loading} = useFetchRejectedTestimonies();
     const [selectedTestimony, setSelectedTestimony] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const handleOpenModal = (testimony) => {
-
         setSelectedTestimony(testimony);
         setIsModalOpen(true);
     };
@@ -19,29 +17,27 @@ const RejectedTestimoniesSection = () => {
         setIsModalOpen(false);
     }
 
-    if (loading) {
-        return <div>Loading...</div>
-    }
+
 
     return (
       <section className="validateTestimonies">
        {testimonies.length > 0 ? (
       <>
-          <p>Vous avez {testimonies.length} avis rejetés actuellement.</p>
+          <p className="validationCount">Vous avez {testimonies.length} avis rejetés actuellement.</p>
           <TestimonyTable
               testimonies={testimonies} 
               onOpenModal={handleOpenModal} 
           />
           {isModalOpen && selectedTestimony && (
               <TestimonyModal
-                  mode="moderation" 
+                  mode="consultation" 
                   testimony={selectedTestimony} 
                   onClose={handleCloseModal} 
               />
           )}
       </>
   ) : (
-      <p>Vous n'avez pas d'avis rejetés actuellement.</p>
+      <p className="validationInfo">Vous n'avez pas d'avis rejetés actuellement.</p>
   )}
      
   </section>
