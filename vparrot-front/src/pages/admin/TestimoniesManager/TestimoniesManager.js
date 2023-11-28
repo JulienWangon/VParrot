c0e0c9316727340e7529e9baf0fdd0419e2befea
Julien Wangon <julien.wangon@gmail.com>
2023-11-28 01:17:02 +0100

    UPDATE : delete all testimonies state management for use context

--- vparrot-front/src/pages/admin/TestimoniesManager/TestimoniesManager.js
+++ vparrot-front/src/pages/admin/TestimoniesManager/TestimoniesManager.js
@@ -1,73 +1,16 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React from 'react';
 import Header from '../../../components/common/Header/Header';
 import ModerationSection from '../../../components/Testimonies/admin/ModerationSection/ModerationSection';
 import H2Title from '../../../components/common/H2Title/H2Title';
 
 import './testimoniesManager.css';
+
 import ValidateTestimoniesSection from '../../../components/Testimonies/admin/ValidateTestimonies/ValidateTestimoniesSection';
 import RejectedTestimoniesSection from '../../../components/Testimonies/admin/RejectedTestimonies/RejectedTestimonies';
-
-import useGetUnmoderatedTestimonies from '../../../components/Testimonies/hooks/useFetchUnmoderatedTestimonies';
-import useFetchRejectedTestimonies from '../../../components/Testimonies/hooks/useFetchRejectedTestimonies';
-import useFetchModeratedTestimonies from '../../../components/Testimonies/hooks/useFetchModeratedTestimonies';
-import useFetchApproveTestimony from '../../../components/Testimonies/hooks/useFetchApproveTestimony';
-import useFetchRejectTestimony from '../../../components/Testimonies/hooks/useFetchRejectTestimony';
 import CreationSection from '../../../components/Testimonies/admin/CreationSection/CreationSection';
 
 const TestimoniesManager = () => { 
 
-    //Etat pour les listes de témoignages
-    const [unmoderatedTestimonies, setUnmoderatedTestimonies] = useState([]);
-    const [moderatedTestimonies, setModeratedTestimonies] = useState([]);
-    const [rejectedTestimonies, setRejectedTestimonies] = useState([]);
-
-    //Récupération des données initiales
-    const { testimonies: initialUnmoderatedTestimonies } = useGetUnmoderatedTestimonies();
-    const { testimonies: initialRejectedTestimonies } = useFetchRejectedTestimonies();
-    const { testimonies: initialModeratedTestimonies } = useFetchModeratedTestimonies();
-
-    const { approveThisTestimony } = useFetchApproveTestimony();
-    const { rejectThisTestimony } = useFetchRejectTestimony();
-
-    useEffect(() => {
-    
-        setUnmoderatedTestimonies(initialUnmoderatedTestimonies.filter(t => !t.isModerated));
-        setModeratedTestimonies(initialModeratedTestimonies);
-        setRejectedTestimonies(initialRejectedTestimonies);
-    }, [initialUnmoderatedTestimonies, initialModeratedTestimonies, initialRejectedTestimonies]);
-
-
-    const handleCreateTestimony = useCallback((newTestimonyData) => {
-        if (newTestimonyData) {
-            setUnmoderatedTestimonies(prevTestimonies => [...prevTestimonies, newTestimonyData]);
-        }
-    }, []);
-    
-
-    const handleApproveTestimony = useCallback((testimonyId) => {
-        approveThisTestimony(testimonyId, () => {
-            setUnmoderatedTestimonies(prevTestimonies => prevTestimonies.filter(t => t.idTestimony !== testimonyId));
-            
-            const approvedTestimony = unmoderatedTestimonies.find(t => t.idTestimony === testimonyId);
-            if (approvedTestimony) {
-                setModeratedTestimonies(prevTestimonies => [...prevTestimonies, approvedTestimony]);
-            }
-        });
-    }, [approveThisTestimony, unmoderatedTestimonies]);
-
-
-
-    const handleRejectTestimony = useCallback((testimonyId) => {
-        rejectThisTestimony(testimonyId, () => {
-            setUnmoderatedTestimonies(prevTestimonies => prevTestimonies.filter(t => t.idTestimony !== testimonyId));
-            
-            const rejectedTestimony = unmoderatedTestimonies.find(t => t.idTestimony === testimonyId);
-            if (rejectedTestimony) {
-                setRejectedTestimonies(prevTestimonies => [...prevTestimonies, rejectedTestimony]);
-            }
-        });
-    }, [rejectThisTestimony, unmoderatedTestimonies]); 
-    
 
 
   
@@ -77,20 +20,16 @@ const TestimoniesManager = () => {
         <Header title="Avis Clients" slogan="Gestion des avis clients"/>
         <main>
 
-            <CreationSection onCreateTestimony={handleCreateTestimony}/>
+            <CreationSection/>
 
             <H2Title h2Text="Vos avis client en attente de modération" className="moderationSectionTitle"/>
-            <ModerationSection
-                testimonies={unmoderatedTestimonies}
-                onApprove={handleApproveTestimony}
-                onReject={handleRejectTestimony}               
-            />
+            <ModerationSection/>
 
             <H2Title h2Text="Vos avis clients publiés" className="validateTestimoniesTitle"/>
-            <ValidateTestimoniesSection  testimonies={moderatedTestimonies}/>
+            <ValidateTestimoniesSection/>
 
             <H2Title h2Text="Vos avis clients rejetés" className="rejectedTestimoniesTitle"/>
-            <RejectedTestimoniesSection  testimonies={rejectedTestimonies}/>
+            <RejectedTestimoniesSection/>
 
         </main>     
     </>
