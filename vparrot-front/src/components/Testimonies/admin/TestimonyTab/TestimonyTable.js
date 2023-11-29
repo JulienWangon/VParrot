51a37b0d86ebdc16009c9d3bd2929e7e37a9ab62
Julien Wangon <julien.wangon@gmail.com>
2023-11-29 22:52:20 +0100

    UPDATE add delete testimony btn

--- vparrot-front/src/components/Testimonies/admin/TestimonyTab/TestimonyTable.js
+++ vparrot-front/src/components/Testimonies/admin/TestimonyTab/TestimonyTable.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import { useTestimonies } from '../../../../contexts/TestimoniesContext';
+
 import './testimonyTable.css';
 
 const TestimonyTable = ({ testimonies, onOpenModal }) => {
+
+  const { deleteTestimony } = useTestimonies();
+
+  const handleDelete = (idTestimony) => {
+    // Confirmer avant la suppression
+    if (window.confirm('Êtes-vous sûr de vouloir supprimer ce témoignage ?')) {
+        deleteTestimony(idTestimony);
+    }
+};
 
 
   return (
@@ -15,6 +26,7 @@ const TestimonyTable = ({ testimonies, onOpenModal }) => {
       <th>Note</th>
       <th className="d-none d-md-table-cell">Commentaire</th>
       <th className="d-table-cell d-md-none">Détails</th>
+      <th className="d-none d-md-table-cell">Supprimer</th>
     </tr>
   </thead>
   <tbody>
@@ -29,6 +41,14 @@ const TestimonyTable = ({ testimonies, onOpenModal }) => {
             <i className="fas fa-eye eyeBtnLogo"></i>
           </button>
         </td>
+        <td className="d-none d-md-table-cell">
+        <button className="btn btn-danger" onClick={() => handleDelete(testimony.idTestimony)}>
+          <i className="fas fa-trash-alt"></i> {/* Icône de suppression */}
+        </button>
+      </td>
+
+        
+        
       </tr>
     ))}
   </tbody>
