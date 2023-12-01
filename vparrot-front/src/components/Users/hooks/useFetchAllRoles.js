0d3738de9ba279f79a7bae8bcc6e4818ff9fd278
Julien Wangon <julien.wangon@gmail.com>
2023-12-01 11:56:31 +0100

    ADD create useFetchAllroles hook

---
+++ vparrot-front/src/components/Users/hooks/useFetchAllRoles.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import { fetchAllRoles } from "../RolesService";
+import { useMessage } from "../../../contexts/MessagesContext";
+
+
+const useFetchAllRoles = () => {
+
+    const [roles, setRoles] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const { showMessage } = useMessage();
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+
+            setIsLoading(true);
+
+            try {
+
+                const response = await fetchAllRoles();
+
+                if(response) {
+                    setRoles(response);
+                }  else {
+
+                  showMessage({ data: "La récupération des utilisateurs a échoué" }, 'error');
+                }
+            } catch (error) {
+
+              if (error.response && error.response.data && error.response.data.status === 'error') {
+                showMessage({ message: error.response.data.message || "Erreur lors de l'opération" }, 'error');
+              } else {
+                showMessage({ message: error.message || "Une erreur s'est produite" }, 'error');
+              }
+            } finally {
+
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [showMessage])
+
+    return { roles, isLoading }
+}
+
+export default useFetchAllRoles;
+
+
