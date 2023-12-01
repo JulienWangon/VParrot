4230038f2ee4960180423b578d6f371c9c79ff34
Julien Wangon <julien.wangon@gmail.com>
2023-12-01 10:46:09 +0100

    ADD create hook to get all users list

---
+++ vparrot-front/src/components/Users/hooks/useFetchAllUsers.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { fetchAllUsers } from "../UsersService";
+import { useMessage } from "../../../contexts/MessagesContext";
+
+const useFetchAllUsers = () => {
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const { showMessage } = useMessage();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetchAllUsers();
+                console.log("la réponse", response);
+                if (response) {
+                    setUsers(response);
+                } else {
+                    showMessage({ message: "La récupération des utilisateurs a échoué" }, 'error');
+                }
+            } catch (error) {
+                if (error.response && error.response.data && error.response.data.status === 'error') {
+                    showMessage({ message: error.response.data.message || "Erreur lors de l'opération" }, 'error');
+                } else {
+                    showMessage({ message: error.message || "Une erreur s'est produite" }, 'error');
+                }
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [showMessage]); 
+
+    return { users, setUsers, isLoading };
+}
+
+export default useFetchAllUsers;
