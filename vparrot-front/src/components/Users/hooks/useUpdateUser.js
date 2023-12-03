8a042db5d9198f8a2ece17e7dce0882ccf0c4151
Julien Wangon <julien.wangon@gmail.com>
2023-12-03 11:45:56 +0100

    ADD create hook for update user

---
+++ vparrot-front/src/components/Users/hooks/useUpdateUser.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { updateUser } from "../UsersService";
+import { useMessage } from "../../../contexts/MessagesContext";
+import { useAuth } from "../../../contexts/AuthContext";
+
+
+const useUpdateUser = () => {
+
+    const { showMessage} = useMessage();
+    const { csrfToken } = useAuth();
+    const [ isLoading, setIsLoading ] = useState(false);
+
+    const updateExistingUser = async (userData) => {
+
+        setIsLoading(true);
+
+        try {
+
+            const response = await updateUser(userData, csrfToken);
+
+            showMessage({data: response }, 'success');
+        } catch (error) {
+
+            if(error.response) {
+
+                showMessage({ data: error.response.data.message }, 'error')
+            } else   {
+
+              showMessage({ data: error || "Une erreur s'est produite" }, 'error');
+            } 
+        } finally {
+              
+          setIsLoading(false);
+      }
+    };
+
+    return { updateExistingUser, isLoading};
+
+}
+
+export default useUpdateUser;
