01895b39323010124f4b8a56bfb6653237494998
Julien Wangon <julien.wangon@gmail.com>
2023-12-04 01:05:22 +0100

    UPDATE : add props for delete user with deleted btn

--- vparrot-front/src/components/Users/UserCard/UserCard.js
+++ vparrot-front/src/components/Users/UserCard/UserCard.js
@@ -3,7 +3,7 @@ import Button from '../../common/Buttons/Button/Button';
 
 import cardStyle from './userCard.module.css';
 
-const UserCard = ({ user, onEditUser }) => {
+const UserCard = ({ user, onEditUser, onDeleteUser }) => {
   return (
     <div className={`${cardStyle.cardUser} card my-3`}>
             <h5 className={`${cardStyle.cardUserHeader} card-header`}>{user.roleName}</h5>
@@ -14,7 +14,7 @@ const UserCard = ({ user, onEditUser }) => {
             </div>
             <div className={`${cardStyle.cardContactFooter} card-footer`}>
                 <Button onClick={() => onEditUser(user)} className={cardStyle.updateUser} colorStyle="redBtn">Modifier</Button>
-                <Button className={cardStyle.deleteUser} colorStyle="redBtn">Supprimer</Button>         
+                <Button onClick={() => onDeleteUser(user.idUser)} className={cardStyle.deleteUser} colorStyle="redBtn">Supprimer</Button>         
             </div>
         </div>
   );
